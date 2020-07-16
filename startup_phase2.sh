65117c214078976d8e2b94f933f66b1270ecb4b2
Ken Ellinwood <kellinwood@yahoo.com>
2020-07-15 14:33:02 -1000

    Add support for a second tool (ltijs+react)

--- startup_phase2.sh
+++ startup_phase2.sh
@@ -3,8 +3,16 @@
 set -e
 cd $(dirname $0)
 
-docker-compose stop lti-tool1
-docker-compose rm --force lti-tool1
-docker-compose build lti-tool1
-docker-compose create lti-tool1
-docker-compose start lti-tool1
+if [[ $# != 1 ]]; then
+    echo "USAGE:   startup_phase2.sh <tool service name>"
+    echo "EXAMPLE: startup_phase2.sh lti-tool1"
+    exit 1
+fi
+
+service=$1
+
+docker-compose stop ${service}
+docker-compose rm --force ${service}
+docker-compose build ${service}
+docker-compose create ${service}
+docker-compose start ${service}
